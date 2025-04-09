211dc07a5f080dd08f58551a742c1ae7f142bf45
GabTrusMat <gabriel.tmateus@gmail.com>
2025-04-08 21:57:28 -0300

    Integração com API Spring Boot

--- App.js
+++ App.js
@@ -1,25 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { Provider as PaperProvider } from 'react-native-paper';
+import AppNavigator from './src/navigation/AppNavigator';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.nome}>Gabriel Trus</Text>
-      <StatusBar style="auto" />
-    </View>
+    <PaperProvider>
+      <AppNavigator />
+    </PaperProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#F5198E',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  nome: {
-    fontSize: 30,
-    fontWeight: 'bold',
-    color: '#03FFFF'
-  }
-});
